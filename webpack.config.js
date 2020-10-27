a46d80affb096996b20ba149ed6997dcc1f63215
Jon Smusev <you@example.com>
2020-10-27 18:47:13 +0200

    template ready and stimulus installed

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/env"],
+                        plugins: ["transform-class-properties"]
+                    }
+                }
+            }
+        ]
+    }
+};
